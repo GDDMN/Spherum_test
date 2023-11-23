398a91779edf0fb77ac6c1113e8fad94db4d319b
GDDMN <apawkin@gmail.com>
2023-11-23 18:05:35 +0300

    add sphere pull on 1-st scene

---
+++ Assets/Scripts/Spheres/SpherePull.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpherePull : MonoBehaviour
+{
+  [SerializeField] private List<Sphere> _allSpheres = new List<Sphere>();
+
+}
+
